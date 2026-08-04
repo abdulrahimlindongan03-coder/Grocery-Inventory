9a98838fbd1e92277c93a34809644ee2e04f68e0
abdulrahimlindongan03-coder <abdulrahimlindongan03@gmail.com>
2026-08-04 21:41:41 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'rml-partner-v1';
+const CACHE_NAME = 'rml-partner-v2';
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
@@ -7,6 +7,7 @@ const ASSETS_TO_CACHE = [
     './icon-512.png'
 ];
 
+// Install event: Cache files
 self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => {
@@ -16,10 +17,12 @@ self.addEventListener('install', (event) => {
     self.skipWaiting();
 });
 
+// Activate event: Clean up old caches
 self.addEventListener('activate', (event) => {
     event.waitUntil(self.clients.claim());
 });
 
+// Fetch event: Serve cached files if offline
 self.addEventListener('fetch', (event) => {
     event.respondWith(
         caches.match(event.request).then((cachedResponse) => {
